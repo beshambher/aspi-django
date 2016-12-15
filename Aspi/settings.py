7f55a69fb38bc8a42adaef18927140df8e73f5b4
beshambher <bishamberc@gmail.com>
2016-12-16 01:24:00 +0530

    added Procfile and runtime

--- Aspi/settings.py
+++ Aspi/settings.py
@@ -132,6 +132,8 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn")
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn aspi-django.wsgi --log-file -
